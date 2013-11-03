c99081bd24d6ddf1cab79a465cd5a3027a056fb1
Tim Lu <timothyclu@gmail.com>
2013-11-03 00:55:12 -0400

    added pins

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,86 @@
+class PinsController < ApplicationController
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pins = Pin.all
+  end
+
+  def show
+  end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def edit
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @pin.destroy
+    redirect_to pins_url
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pin
+      @pin = Pin.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def pin_params
+      params.require(:pin).permit(:description)
+    end
+end
+
+  # PATCH/PUT /pins/1
+  # PATCH/PUT /pins/1.json
+  def update
+    respond_to do |format|
+      if @pin.update(pin_params)
+        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /pins/1
+  # DELETE /pins/1.json
+  def destroy
+    @pin.destroy
+    respond_to do |format|
+      format.html { redirect_to pins_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pin
+      @pin = Pin.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def pin_params
+      params.require(:pin).permit(:description)
+    end
+end
