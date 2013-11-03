21369e85803add3c915b0abdf3a6e5fbd90318c8
Tim Lu <timothyclu@gmail.com>
2013-11-02 23:13:11 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cliquelive::Application.config.secret_key_base = 'b50956e78c1106833c023860848f5ddf5329fd80fef418b3b32b78efac0dd6edc5de856a8720783025d00c9fe2f47d5650b2ecf1b54365e574c1f8bc8c1e9d31'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cliquelive::Application.config.session_store :cookie_store, key: '_cliquelive_session'
